a74b2e91345cc610055aceb7d12cd560090247cd
zrcni <zrcni9@gmail.com>
2025-10-20 10:46:01 +0300

    make SagaLogger calls chainable

--- src/sagas/SagaLogger.ts
+++ src/sagas/SagaLogger.ts
@@ -7,65 +7,80 @@ export class SagaLogger {
     this.sagaOrchestrator = sagaOrchestrator
   }
 
-  onSagaStarted(listener: (event: SagaOrchestratorEvents["sagaStarted"]) => void) {
+  onSagaStarted(
+    listener: (event: SagaOrchestratorEvents["sagaStarted"]) => void
+  ): this {
     this.sagaOrchestrator.on("sagaStarted", listener)
+    return this
   }
 
   onSagaSucceeded(
     listener: (event: SagaOrchestratorEvents["sagaSucceeded"]) => void
-  ) {
+  ): this {
     this.sagaOrchestrator.on("sagaSucceeded", listener)
+    return this
   }
 
-  onSagaFailed(listener: (event: SagaOrchestratorEvents["sagaFailed"]) => void) {
+  onSagaFailed(
+    listener: (event: SagaOrchestratorEvents["sagaFailed"]) => void
+  ): this {
     this.sagaOrchestrator.on("sagaFailed", listener)
+    return this
   }
 
   onTaskStarted(
     listener: (event: SagaOrchestratorEvents["taskStarted"]) => void
-  ) {
+  ): this {
     this.sagaOrchestrator.on("taskStarted", listener)
+    return this
   }
 
   onTaskSucceeded(
     listener: (event: SagaOrchestratorEvents["taskSucceeded"]) => void
-  ) {
+  ): this {
     this.sagaOrchestrator.on("taskSucceeded", listener)
+    return this
   }
 
   onTaskFailed(
     listener: (event: SagaOrchestratorEvents["taskFailed"]) => void
-  ) {
+  ): this {
     this.sagaOrchestrator.on("taskFailed", listener)
+    return this
   }
 
   onCompensationStarted(
     listener: (event: SagaOrchestratorEvents["compensationStarted"]) => void
-  ) {
+  ): this {
     this.sagaOrchestrator.on("compensationStarted", listener)
+    return this
   }
 
   onCompensationSucceeded(
     listener: (event: SagaOrchestratorEvents["compensationSucceeded"]) => void
-  ) {
+  ): this {
     this.sagaOrchestrator.on("compensationSucceeded", listener)
+    return this
   }
 
   onCompensationFailed(
     listener: (event: SagaOrchestratorEvents["compensationFailed"]) => void
-  ) {
+  ): this {
     this.sagaOrchestrator.on("compensationFailed", listener)
+    return this
   }
 
   onMiddlewareSucceeded(
     listener: (event: SagaOrchestratorEvents["middlewareSucceeded"]) => void
-  ) {
+  ): this {
     this.sagaOrchestrator.on("middlewareSucceeded", listener)
+    return this
   }
 
   onMiddlewareFailed(
     listener: (event: SagaOrchestratorEvents["middlewareFailed"]) => void
-  ) {
+  ): this {
     this.sagaOrchestrator.on("middlewareFailed", listener)
+    return this
   }
 }
